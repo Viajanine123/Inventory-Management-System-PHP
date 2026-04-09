e8cf3fec089ef54f9466c04326fb878079483194
Viajanine123 <viajanine13@gmail.com>
2026-04-09 12:47:15 +0800

    Update database name to inventorymanagement

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'final_inventory_db');
+$db = mysqli_connect('localhost', 'root', '', 'inventorymanagement');
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
